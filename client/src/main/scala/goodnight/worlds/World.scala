12238cf5c3c6440bd407a5ea39ec3cc5a1ec3c46
Florian Büther <buether@itm.uni-luebeck.de>
2019-09-04 14:15:26 +0200

    Added remaining resources, added temporary work-in-progress files.

---
+++ client/src/main/scala/goodnight/worlds/World.scala
@@ -0,0 +1,13 @@
+
+// package goodnight.worlds
+
+// // import japgolly.scalajs.react.ReactElement
+
+// import goodnight.components.Shell
+
+
+
+// object World
+// {
+//   val render = Shell("props")
+// }
