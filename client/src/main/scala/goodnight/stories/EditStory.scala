878c820a75180024f4d026f4a57db49f0a780397
Florian Büther <fbuether@jasminefields.net>
2019-10-29 23:01:38 +0100

    Starting work on story editing.

--- client/src/main/scala/goodnight/stories/EditStory.scala
+++ client/src/main/scala/goodnight/stories/EditStory.scala
@@ -21,10 +21,30 @@ import goodnight.model
 import goodnight.common.api.Story._
 
 object EditStory {
+  type Props = (pages.Router, String)
+
+  val component = ScalaComponent.builder[Props]("EditStory").
+    render_P(p =>
+      <.div(
+        <.h2("Edit Story: " + p._2),
+        <.p("This is where you edit your story."),
+        <.h3("Edit Metadata"),
+        <.h3("Edit Locations"),
+        <.h3("Edit Qualities"),
+        <.h3("Edit Scenes"))).
+    build
+
+  def loadStory(router: pages.Router, name: String): AsyncCallback[VdomElement] =
+    Request(ApiV1.Story, name).send.forJson.map({
+      case Reply(200, Success(storyJson)) =>
+        this.component(router, name)
+      case e =>
+        <.div("Error :( -> " + e)
+    })
 
 
   def render(page: pages.EditStory, router: pages.Router) =
     Shell.component(router)(
-      Banner.component(router, "Alien World.png", "World:"+page.name))
-
+      Banner.component((router, "Alien World.png", "World:"+page.name)),
+      Loading.suspend(router, loadStory(router, page.name)))
 }
