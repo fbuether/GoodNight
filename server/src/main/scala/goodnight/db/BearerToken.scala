a575e7de8e825d0828f10e2447ed7e23da6ccb27
Florian Büther <fbuether@jasminefields.net>
2019-11-04 00:10:20 +0100

    Created reminder file that bearer tokens need to be stored in database.

---
+++ server/src/main/scala/goodnight/db/BearerToken.scala
@@ -0,0 +1,25 @@
+
+package goodnight.db
+
+// import java.util.UUID
+// import slick.jdbc.PostgresProfile.api._
+
+// import goodnight.server.PostgresProfile._
+// import goodnight.server.PostgresProfile.Table
+// import goodnight.model
+
+
+// class LoginAuth(tag: Tag) extends Table[model.LoginAuth](tag, "login_auth") {
+//   def id = column[UUID]("id", O.PrimaryKey)
+//   def providerID = column[String]("provider_id")
+//   def providerKey = column[String]("provider_key")
+//   def hasher = column[String]("hasher")
+//   def password = column[String]("password")
+//   def salt = column[Option[String]]("salt")
+//   def * = ((id, providerID, providerKey, hasher, password, salt) <>
+//     (model.LoginAuth.tupled, model.LoginAuth.unapply))
+// }
+
+// object LoginAuth {
+//   def apply() = TableQuery[LoginAuth]
+// }
