5724e5f5a9ad1e455739ef5a4115af54ef0061d0
Florian Büther <fbuether@jasminefields.net>
2019-11-24 19:37:48 +0100

    Again reworked the sceneparser, has an acceptable shape now.

--- common/src/main/scala/goodnight/logic/SceneParser.scala
+++ common/src/main/scala/goodnight/logic/SceneParser.scala
@@ -10,102 +10,64 @@ import goodnight.model.text._
 
 
 object SceneParser {
+  // single lines, detected by start of the line.
+  private def contentLine[_:P]: P[Either[String, String]] =
+    P(!">" ~ !"$" ~/ CharsWhile(_ != '\n', 0).!).
+      map(line => Left(line.trim))
 
-  private def whitespace[_:P] = P(CharsWhileIn(" \n\t", 0))
-  private def inlineWhitespace[_:P] = P(CharsWhileIn(" \t", 0))
+  private def choiceLine[_:P]: P[String] =
+    P(">" ~/ CharsWhile(_ != '\n', 0).!).
+      map(line => line.trim)
 
-  private def line[_:P] = P(CharsWhile(_ != '\n').!).
-    map(_.trim)
-  private def lineEnd[_:P] = P("\n" | End)./
+  private def settingLine[_:P]: P[Either[String, String]] =
+    P("$" ~/ CharsWhile(_ != '\n', 0).!).
+      map(line => Right(line.trim))
 
+  // a group of content and settings
+  private def partContent[_:P]: P[(String, Seq[String])] =
+    P((contentLine | settingLine).rep(min = 1, sep = "\n")).
+      map({ lines =>
+        val (lefts, rights) = lines.partition(_.isLeft)
+        (lefts.map(_.left.get).mkString("\n"),
+          rights.map(_.right.get).toList)
+      })
 
+  // a new choice, as indicated by leading ">" on first line
+  private def choice[_:P]: P[PChoice] =
+    P(choiceLine ~ ("\n" ~ partContent).?).
+      map({ case (firstText, remainder) =>
+        val (text, settings) = remainder.getOrElse("", Seq())
+        val content = firstText + (if (text.isEmpty) "" else "\n" + text)
+        PChoice(content, settings)
+      })
 
-  private def textLine[_:P] = P(!">" ~ line)
+  // a list of several choices
+  private def choices[_:P]: P[Seq[PChoice]] =
+    P(choice.rep(min = 0, sep = "\n"))
 
+  private def sceneOnlyChoices[_:P] = P(choices).
+    map(PScene("", Seq(), _))
 
+  private def sceneContentAndMaybeChoices[_:P] =
+    P(partContent ~ ("\n" ~ choices).?).
+      map(c => PScene(c._1, c._2, c._3.getOrElse(Seq())))
 
-  // private def paragraph[_:P]: P[Paragraph] =
-  //   P(textLine.rep(min = 1, sep = "\n"./)).
-  //     map(lines => Paragraph(lines.mkString(" ")))
-
-  private def paragraph[_:P]: P[Paragraph] =
-    P((!">" ~ CharsWhile(_ != '\n', 1).!).rep(min = 1, sep = "\n")).
-      map(lines => Paragraph(lines.mkString(" ")))
-
-
-
-  // private def blockElements[_:P]: P[Seq[BlockElement]] =
-  //   P(paragraph.rep(sep = "\n".rep(min = 2, sep = CharsWhileIn(" \t", 0))))
-
-
-
-  private def markdown[_:P]: P[Markdown] = P(
-    paragraph
-    // whitespace ~/ blockElements ~ whitespace
-  ).
-    map(p => Markdown(Seq(p)))
-
-  // def markdown = paragraph
-
-
-  // inline elements
-
-
-
-
-  // instructions
-  // private def instructionPrefix[_:P] = P(">" ~/ inlineWhitespace)
-
-  // private def option[_:P] = P(instructionPrefix ~/ line ~ lineEnd ~ markdown).
-  //   map(ParsedOption.tupled)
+  // a full scene, composed of content and possibly choices or just choices
+  private def scene[_:P]: P[PScene] =
+    P((sceneContentAndMaybeChoices | sceneOnlyChoices) ~ End)
 
 
-  private def option[_:P]: P[ParsedOption] =
-    P(">" ~ CharsWhile(_ != '\n', 1).! ~ "\n" ~ markdown).
-      map(ParsedOption.tupled)
+  case class PChoice(content: String, settings: Seq[String])
+  case class PScene(content: String, settings: Seq[String],
+    choices: Seq[PChoice])
 
 
-  private def options[_:P]: P[Seq[ParsedOption]] =
-    P(option.rep(min = 0, sep = "\n"))
-
-
-
-  def scene[_:P] =
-    P(whitespace ~ CharsWhileIn("#", 0) ~ line ~ lineEnd ~/
-      whitespace ~ markdown ~
-      whitespace ~ options ~ End).
-      map(ParsedScene.tupled)
-
-
-
-  case class ParsedOption(
-    title: String,
-    text: Markdown
-  )
-
-  case class ParsedScene(
-    title: String,
-    text: Markdown,
-    options: Seq[ParsedOption]
-  )
-
-
-
-
-  def parseMarkdown(raw: String): Either[String, Markdown] =
-    fastparse.parse(raw, markdown(_), verboseFailures = true) match {
-      case Parsed.Success(md, _) => Right(md)
-      case failure @ Parsed.Failure(_, _, _) => Left(failure.msg)
-    }
-
-
-  def parseScene(raw: String): Either[String, ParsedScene] = {
+  def parsePScene(raw: String, detailedErrors: Boolean = false) =
     fastparse.parse(raw, scene(_), verboseFailures = true) match {
       case Parsed.Success(value, _) => Right(value)
       case failure @ Parsed.Failure(_, _, _) =>
-        // Left(failure.msg)
-        Left(failure.trace().longAggregateMsg)
+        if (detailedErrors) Left(failure.trace().longAggregateMsg)
+        else Left(failure.msg)
     }
-  }
 }
 
--- common/src/test/scala/goodnight/logic/SceneParserTest.scala
+++ common/src/test/scala/goodnight/logic/SceneParserTest.scala
@@ -11,147 +11,124 @@ import goodnight.logic.SceneParser._
 
 
 class SceneParserTest extends FunSpec with Inside {
-  val parsed: (String => SceneParser.ParsedScene) =
-    SceneParser.parseScene(_) match {
+  val parsed: (String => SceneParser.PScene) =
+    SceneParser.parsePScene(_, true) match {
       case Right(scene) => scene
       case Left(error) => throw new Error(s"Parsing failed with: $error")
     }
 
-  val error: (String => String) =
-    SceneParser.parseScene(_) match {
-      case Left(error) => error
-      case Right(scene) => throw new Error(s"Parsing suceeded with: $scene")
+  describe("scene content") {
+    it("can be a single line") {
+      assert(parsed("main").content ==
+        "main")
     }
 
-  describe("scene titles") {
-    it("are any first line") {
-      val text = "first line\nsome text\nwell."
-      assert(parsed(text).title == "first line")
+    it("can be several lines") {
+      assert(parsed("main\nand\nmore").content ==
+        "main\nand\nmore")
     }
 
-    it("are the only line, if nothing else is there") {
-      val text = "only a title"
-      assert(parsed(text).title == text)
+    it("can be empty") {
+      assert(parsed("").content ==
+        "")
     }
 
-    it("may contain any kind of character") {
-      val text = "¬!\"£$%^&*()_+`[]{};\'#:@~,./<?>\\|"
-      assert(parsed(text).title == text)
+    it("can have empty lines") {
+      assert(parsed("main\n\nmore").content ==
+        "main\n\nmore")
     }
 
-    describe("strip the first uninterrupted group of hashes") {
-      it("which works for one hash") {
-        val text = "#title"
-        assert(parsed(text).title == "title")
-      }
-
-      it("works for a hash and a space") {
-        val text = "# title"
-        assert(parsed(text).title == "title")
-      }
-
-      it("works for multiple hashes") {
-        val text = "#### title"
-        assert(parsed(text).title == "title")
-      }
-
-      it("preserves hashes after anything else") {
-        val text = "## ## title"
-        assert(parsed(text).title == "## title")
-      }
+    it("can have many empty lines") {
+      assert(parsed("main\n\nand\n\n\nmore\n\n\n\n").content ==
+        "main\n\nand\n\n\nmore\n\n\n\n")
     }
 
-    it("trim surrounding whitespace") {
-      val text = "#  title\t\t \nbody"
-      assert(parsed(text).title == "title")
+    it("ignores lines with $") {
+      assert(parsed("""|main
+                       |$something
+                       |
+                       |or other""".stripMargin).content ==
+        "main\n\nor other")
     }
 
-    it("may contain preceeding line breaks and hashes") {
-      val text ="\n\n## title\n\nbody"
-      assert(parsed(text).title == "title")
-    }
-
-    it("may contain preceeding line breaks") {
-      val text ="\n\ntitle\n\nbody"
-      assert(parsed(text).title == "title")
-    }
-
-    it("may contain whitespace in front of the hash") {
-      val text = " # title"
-      assert(parsed(text).title == "title")
-    }
-
-    describe("may not") {
-      it("be empty") {
-        val text = ""
-        assert(error(text).startsWith("Position 1:1"))
-      }
-
-      it("be empty even if a body exists") {
-        val text = "##\n\nbody"
-        assert(error(text).startsWith("Position 1:3"))
-      }
+    it("ignores everything after a line with >") {
+      assert(parsed("""|main
+                       |>choice
+                       |$setting
+                       |body""".stripMargin).content ==
+        "main")
     }
   }
 
-  describe("scene texts") {
-    it("are anything after the first line") {
-      val text = "# head\nbody"
-      assert(parsed(text).text == Markdown(Seq(Paragraph("body"))))
+  describe("scene settings") {
+    they("start with $ sign") {
+      assert(parsed("$setting").settings ==
+        Seq("setting"))
     }
 
-    describe("may be empty if") {
-      it("there is just one line") {
-        val text = "head"
-        assert(parsed(text).text == Markdown(Seq()))
-      }
-
-      it("there is just whitespace") {
-        val text ="head\n  "
-        assert(parsed(text).text == Markdown(Seq()))
-      }
-
-      it("there is several all-empty lines") {
-        val text = "head\n\n  \n \t  \n \t  "
-        assert(parsed(text).text == Markdown(Seq()))
-      }
+    they("can appear multiple times") {
+      assert(parsed("$setting\nbody\n$more").settings ==
+        Seq("setting", "more"))
     }
 
-    it("may span several lines") {
-      val text = "head\nbody 1\nbody 2"
-      assert(parsed(text).text == Markdown(Seq(Paragraph("body 1 body 2"))))
+    they("do not contain empty lines") {
+      assert(parsed("$setting\n\n$more").settings ==
+        Seq("setting", "more"))
     }
 
-    it("gets whitespace trimmed per line") {
-      val text = "head\n  body   \t \n text \n \n \n"
-      assert(parsed(text).text == Markdown(Seq(Paragraph("body text"))))
-    }
-
-    it("may not contain option text") {
-      val text = "#title\nbody\n>f"
-      assert(parsed(text).text == Markdown(Seq(Paragraph("body"))))
+    they("can be empty") {
+      assert(parsed("$\n$more\n$\nwell.").settings ==
+        Seq("", "more", ""))
     }
   }
 
-  describe("scene options") {
-    they("may be present") {
-      val text = """|#head
-                    |body
-                    |>option
-                    |obody""".stripMargin
-      assert(parsed(text) ==
-        ParsedScene("head",
-          Markdown(Seq(Paragraph("body"))),
-          Seq(ParsedOption("option",
-            Markdown(Seq(Paragraph("obody")))))))
+  describe("scene choices") {
+    they("start with the > sign") {
+      assert(parsed("body\n>choice") ==
+        PScene("body", Seq(),
+          Seq(PChoice("choice", Seq()))))
     }
-  }
 
-  describe("whitespace in the text") {
-    it("may be inbetween title and text") {
-      val text = "#title\n\n\n\n\ntext"
-      assert(parsed(text).title == "title")
-      assert(parsed(text).text == Markdown(Seq(Paragraph("text"))))
+    they("can be the whole body") {
+      assert(parsed(">") ==
+        PScene("", Seq(), Seq(PChoice("", Seq()))))
+    }
+
+    describe("consume all text past the first >") {
+      they("as body") {
+        assert(parsed(">this\nis\n\ncontent").choices(0).content ==
+          "this\nis\n\ncontent")
+      }
+
+      they("as settings") {
+        assert(parsed(">\n$set\n$set2").choices(0).settings ==
+          Seq("set", "set2"))
+      }
+
+      they("as settings and body") {
+        assert(parsed(">body\n$set\n$set2\nbody2\n\nbody3").choices(0) ==
+          PChoice("body\nbody2\n\nbody3", Seq("set", "set2")))
+        }
+    }
+
+    they("can occur multiple times") {
+      assert(parsed(">apple\n>orange") ==
+        PScene("", Seq(), Seq(PChoice("apple", Seq()),
+          PChoice("orange", Seq()))))
+    }
+
+    they("can occur after content and settings") {
+      assert(parsed("""|body
+                       |$settings
+                       |more body
+                       |>choice""".stripMargin).choices ==
+        Seq(PChoice("choice", Seq())))
+    }
+
+    they("consume their own choices, but not earlier ones") {
+      assert(parsed("body\n$bodysetting\n>choice\n$setting").
+        choices(0).settings ==
+        Seq("setting"))
     }
   }
 }
