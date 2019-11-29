ec243afb97fb6116d03ba03c64c34c0d0df23f72
Florian Büther <fbuether@jasminefields.net>
2019-11-29 19:42:23 +0100

    Removed unneccessary imports.

--- server/src/main/scala/goodnight/stories/write/Scenes.scala
+++ server/src/main/scala/goodnight/stories/write/Scenes.scala
@@ -1,22 +1,12 @@
 
 package goodnight.stories.write
 
-import java.util.UUID
-import play.api.db.slick.DbName
-import play.api.libs.functional.syntax._
-import play.api.libs.json.{ JsValue, Json, Reads, JsPath, JsArray }
-import play.api.mvc.AnyContent
-import play.api.mvc.BaseController
 import play.api.mvc.ControllerComponents
-import play.api.mvc.Request
-import play.api.mvc.Result
 import scala.concurrent.ExecutionContext
-import scala.concurrent.Future
 import slick.jdbc.PostgresProfile.api._
 import upickle.default.macroRW
 
 import goodnight.api.authentication.AuthService
-import goodnight.api.authentication.Id
 import goodnight.common.Serialise._
 import goodnight.db
 import goodnight.model
--- server/src/main/scala/goodnight/stories/write/Stories.scala
+++ server/src/main/scala/goodnight/stories/write/Stories.scala
@@ -2,27 +2,17 @@
 package goodnight.stories.write
 
 import java.util.UUID
-import play.api.db.slick.DbName
-import play.api.libs.functional.syntax._
-import play.api.libs.json.{ JsValue, Json, Reads, JsPath, JsArray }
-import play.api.mvc.AnyContent
-import play.api.mvc.BaseController
 import play.api.mvc.ControllerComponents
-import play.api.mvc.Request
-import play.api.mvc.Result
 import scala.concurrent.ExecutionContext
-import scala.concurrent.Future
 import slick.jdbc.PostgresProfile.api._
 import upickle.default.macroRW
 
 import goodnight.api.authentication.AuthService
-import goodnight.api.authentication.Id
 import goodnight.common.Serialise._
 import goodnight.db
 import goodnight.model
 import goodnight.server.Controller
 import goodnight.server.PostgresProfile.Database
-import goodnight.logic.SceneParser
 
 
 class Stories(components: ControllerComponents,
