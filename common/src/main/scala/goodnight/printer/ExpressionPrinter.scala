0efa55d933450fe3db5b0c94396bdaf9e64fa7e9
Florian Büther <fbuether@jasminefields.net>
2020-01-16 09:42:52 +0100

    Print expressions, evaluate them.

--- common/src/main/scala/goodnight/printer/ExpressionPrinter.scala
+++ common/src/main/scala/goodnight/printer/ExpressionPrinter.scala
@@ -33,4 +33,31 @@ object ExpressionPrinter {
     case Binary(op, left, right) =>
       print(left) + print(op) + print(right)
   }
+
+  private def toTest(op: BinaryOperator): String = op match {
+    case Add => "plus"
+    case Sub => "minus"
+    case Mult => "times"
+    case Div => "divided by"
+
+    case And => " and "
+    case Or => " or "
+
+    case Greater => "more than"
+    case GreaterOrEqual => "at least"
+    case Less => "less than"
+    case LessOrEqual => "at most"
+    case Equal => "is"
+    case NotEqual => "is not"
+  }
+
+  def toTest(e: Expression): String = e match {
+    case Text(_) | Number(_) => print(e)
+    case Unary(Not, e) => "not " + toTest(e)
+    case Binary(op, left, right) =>
+      val lr = toTest(left)
+      val rr = toTest(right)
+      val l = if (lr.contains(" ")) ("(" + lr + ")") else lr
+      val r = if (rr.contains(" ")) ("(" + rr + ")") else rr
+      l + " " + toTest(op) + " " + r }
 }
--- server/src/main/scala/goodnight/stories/read/Activity.scala
+++ server/src/main/scala/goodnight/stories/read/Activity.scala
@@ -5,8 +5,8 @@ import java.util.UUID
 import slick.jdbc.PostgresProfile.api._
 import scala.concurrent.ExecutionContext
 
-
 import goodnight.db
+import goodnight.model
 
 
 object Activity {
@@ -63,5 +63,59 @@ object Activity {
             as._1,
             scene))))
 
+  }
+
+
+  def evaluateTest(states: Seq[(db.model.State, db.model.Quality)],
+    qualities: Seq[db.model.Quality],
+    expr: model.Expression):
+      Option[model.Expression.Value] = {
+    import goodnight.model.Expression._
+    import goodnight.model.Expression.Value._
+    expr match {
+      case Text(name) =>
+        // we have to infer the type depending on the referred quality
+        states.find(_._2.urlname == name).
+          map(state => (state._2.sort, state._1.value)) match {
+            case Some((db.model.Sort.Bool, v)) => Some(Bool(v == "true"))
+            case Some((db.model.Sort.Integer, v)) =>
+              try { Some(Integer(v.toInt)) } catch { case _: Exception => None }
+            case None => qualities.find(_.urlname == name).map(_.sort) match {
+              case Some(db.model.Sort.Bool) => Some(Bool(false))
+              case Some(db.model.Sort.Integer) => Some(Integer(0))
+              case _ => None } }
+      case Number(n) => Some(Integer(n))
+      case Unary(Not, e) => evaluateTest(states, qualities, e) match {
+        case Some(Bool(b)) => Some(Bool(!b))
+        case Some(Integer(_)) => None
+        case None => None }
+      case Binary(op, e1, e2) =>
+        val v1 = evaluateTest(states, qualities, e1)
+        val v2 = evaluateTest(states, qualities, e2)
+        (v1, v2) match {
+          case (Some(Integer(v1)), Some(Integer(v2))) => op match {
+            case Add  => Some(Integer(v1 + v2))
+            case Sub  => Some(Integer(v1 - v2))
+            case Mult => Some(Integer(v1 * v2))
+            case Div  => Some(Integer(v1 / v2))
+            case And | Or => None
+            case Greater        => Some(Bool(v1 > v2))
+            case GreaterOrEqual => Some(Bool(v1 >= v2))
+            case Less           => Some(Bool(v1 < v2))
+            case LessOrEqual    => Some(Bool(v1 <= v2))
+            case Equal          => Some(Bool(v1 == v2))
+            case NotEqual       => Some(Bool(v1 != v2)) }
+          case (Some(Bool(v1)), Some(Bool(v2))) => op match {
+            case Add | Sub | Mult | Div => None
+            case And => Some(Bool(v1 && v2))
+            case Or  => Some(Bool(v1 || v2))
+            case Greater        => Some(Bool(v1 > v2))
+            case GreaterOrEqual => Some(Bool(v1 >= v2))
+            case Less           => Some(Bool(v1 < v2))
+            case LessOrEqual    => Some(Bool(v1 <= v2))
+            case Equal          => Some(Bool(v1 == v2))
+            case NotEqual       => Some(Bool(v1 != v2)) }
+          case _ => None }
+    }
   }
 }
