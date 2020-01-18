67872229cbd029b57e1aeaba3f86c86246b260a9
Florian Büther <fbuether@jasminefields.net>
2020-01-18 10:55:47 +0100

    Updating stories.

--- server/src/main/scala/goodnight/stories/write/Scene.scala
+++ server/src/main/scala/goodnight/stories/write/Scene.scala
@@ -51,52 +51,23 @@ class Scene(components: ControllerComponents,
       yield result[model.edit.Scene](Ok, Convert.edit(dbScene))))
 
 
-
-
-  private def dbSceneOf(scene: model.Scene): db.model.Scene =
-    db.model.Scene(UUID.randomUUID(),
-      scene.story,
-      scene.raw,
-      scene.name,
-      scene.urlname,
-      scene.text)
-
-  // def createScene(storyUrlname: String) =
-  //   // todo: check if the data actually got inserted.
-  //   auth.SecuredAction.async(parse.text)(request =>
-  //     database.run(
-  //       GetOrNotFound(db.Story.ofUrlname(storyUrlname)).flatMap(story =>
-  //         SceneParser.parseScene(story.urlname, request.body) match {
-  //           case Right(scene) =>
-  //             val dbScene = dbSceneOf(scene)
-  //             db.Scene.insert(dbScene).map(_ =>
-  //               Accepted)
-  //           case Left(error) =>
-  //             DBIO.successful(Conflict(error))
-  //         })))
-
-
-  private def updateScene(oldScene: db.model.Scene, newScene: model.Scene) =
-    db.model.Scene(oldScene.id,
-      oldScene.story,
-      newScene.raw,
-      newScene.name,
-      newScene.urlname,
-      newScene.text)
+  def sceneOf(oldScene: db.model.Scene, raw: String):
+      Either[String, db.model.Scene] =
+    SceneParser.parseScene(oldScene.story, raw.replace("\r\n", "\n")).
+      flatMap(scene =>
+        if (scene.urlname != oldScene.urlname)
+          Left("The name of the scene must not be changed.")
+        else
+          Right(oldScene.copy(
+            raw = scene.raw,
+            text = scene.text)))
 
   def saveScene(storyUrlname: String, sceneUrlname: String) =
-    // todo: check if the data actually got inserted.
     auth.SecuredAction.async(parse.text)(request =>
-      database.run(
-        GetOrNotFound(db.Story.ofUrlname(storyUrlname)).flatMap(story =>
-          GetOrNotFound(db.Scene.named(storyUrlname, sceneUrlname)).
-            flatMap(scene =>
-              SceneParser.parseScene(story.urlname, request.body) match {
-                case Right(newScene) if newScene.urlname == scene.urlname =>
-                  db.Scene().insertOrUpdate(updateScene(scene, newScene)).
-                    map(_ =>
-                      Accepted)
-                case Left(error) =>
-                  DBIO.successful(Conflict(error))
-              }))))
+      database.run(for (
+        oldScene <- GetOrNotFound(db.Scene.named(storyUrlname, sceneUrlname));
+        newScene <- GetOrEither(BadRequest.apply: String => Result)(
+          DBIO.successful(sceneOf(oldScene, request.body)));
+        _ <- db.Scene.update(newScene))
+      yield result[model.edit.Scene](Ok, Convert.edit(newScene))))
 }
